022e6a2cc956d0edfc10b604acbe4d4cf5a35372
Georeg Lucas <lucas@excelinformatica.com>
2024-11-20 22:47:58 +0000

    Adicionado .env e node_modules no .gitignore

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://35.202.163.227:3000/api/v1/user";
-export const JOB_API_END_POINT="http://35.202.163.227:3000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://35.202.163.227:3000/api/v1/application";
-export const COMPANY_API_END_POINT="http://35.202.163.227:3000/api/v1/company";
-export const REACT_APP_API_URL="http://35.202.163.227:3000";
+export const USER_API_END_POINT="http://locahost:8000/api/v1/user";
+export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const REACT_APP_API_URL="http://localhost:8000";
