1a53c061f900cb4589cb9b10b7abc8a7e19721d8
lucas.goncalves <lucas.goncalves@govone.digital>
2024-11-21 01:38:27 -0300

    alteração de localhost por https://portaljob.onrender.com/

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
-export const REACT_APP_API_URL="http://localhost:8000";
+export const USER_API_END_POINT="https://portaljob.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://portaljob.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://portaljob.onrender.comapi/v1/application";
+export const COMPANY_API_END_POINT="https://portaljob.onrender.comapi/v1/company";
+export const REACT_APP_API_URL="https://portaljob.onrender.com";
